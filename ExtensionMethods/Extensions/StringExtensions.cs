caf7e4042e62ea8a4a5988de7c605665124cd8f8
Bach Ngoc Toan <ngoctoan@live.com>
2019-08-14 22:03:57 +0700

    Extension methods

---
+++ ExtensionMethods/Extensions/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace ExtensionMethods.Extensions
+{
+    public static class StringExtensions
+    {
+        public static double ToDouble(this string data)
+        {
+            double result = double.Parse(data);
+            return result;
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using ExtensionMethods.Extensions;
+
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee() { Id = 1, Name = "A"},
+                new Employee() { Id = 2, Name = "B"},
+            };
+           
+            Console.WriteLine(developers.Count());
+            Console.Read();
+        }
+    }
+}
