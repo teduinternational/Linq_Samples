a979eb8e3acbb84f5870421f78eb67f22e8c1006
Bach Ngoc Toan <ngoctoan@live.com>
2019-08-14 22:55:52 +0700

    Custom filter operator

---
+++ CustomFilterOperator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomFilterOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Student collection
+            IList<Student> studentList = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John", Age = 13} ,
+                new Student() { StudentID = 2, StudentName = "Moin",  Age = 21 } ,
+                new Student() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 20} ,
+                new Student() { StudentID = 5, StudentName = "Ron" , Age = 15 }
+            };
+            var result = studentList.Filter(s => s.Age > 12);
+
+            foreach (var student in result)
+            {
+                Console.WriteLine(student.StudentName);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ CustomFilterOperator/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomFilterOperator
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+
+        public string StudentName { get; set; }
+
+        public int Age { get; set; }
+    }
+}
