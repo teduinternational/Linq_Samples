a70f55cd884d5817109ebbcb40e8abbb04355ffe
Bach Ngoc Toan <ngoctoan@live.com>
2019-08-14 22:28:19 +0700

    Function and action

---
+++ FuncVsAct/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FuncVsAct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int> myAction = new Action<int>(DoSomething);
+            myAction(123);
+
+            Func<int,double> myFunc = new Func<int, double>(CalculateSomething);
+            Console.WriteLine(myFunc(5));
+            Console.Read();
+        }
+
+        static void DoSomething(int i)
+        {
+            Console.WriteLine(i);
+        }
+
+        static double CalculateSomething(int i)
+        {
+            return (double) i / 2;
+        }
+    }
+}
