96f7897693be0d0710855f82de3a0c6860f8e377
Bach Ngoc Toan <ngoctoan@live.com>
2019-08-15 23:47:58 +0700

    Sorting operator

---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+
+        public string StudentName { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ SortingOperators/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Models;
+
+namespace SortingOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<Student> studentList = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John", Age = 18 } ,
+                new Student() { StudentID = 2, StudentName = "Steve",  Age = 15 } ,
+                new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 } ,
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 } ,
+                new Student() { StudentID = 5, StudentName = "Ron" , Age = 19 }
+            };
+
+            //Query
+            var orderedByResult = from s in studentList
+                                  orderby s.StudentName ,s.Age descending
+                                  select s;
+
+            var orderedByResult1 = studentList.OrderBy(s => s.StudentName).ThenByDescending(s=>s.Age);
+            foreach (var s in orderedByResult)
+            {
+                Console.WriteLine(s.StudentName);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
