5697694c271888db7738d859e44c97cdcd4ed1f6
Bach Ngoc Toan <ngoctoan@live.com>
2019-08-16 08:23:40 +0700

    Concatenation operator

---
+++ ConcatenationOperator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConcatenationOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> collection1 = new List<string>() { "One", "Two", "Three" };
+            IList<string> collection2 = new List<string>() { "Five", "Six" };
+
+            var collection3 = collection1.Concat(collection2);
+            foreach (string str in collection3)
+                Console.WriteLine(str);
+            Console.Read();
+        }
+    }
+}
