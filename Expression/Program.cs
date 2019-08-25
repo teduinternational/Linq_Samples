d2cca4dc13ee1457f87609e352cef81612c0566f
Bach Ngoc Toan <ngoctoan@live.com>
2019-08-25 22:46:09 +0700

    Expression

---
+++ Expression/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Expression
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<Student, bool> isTeenAger = s => s.Age > 12 && s.Age < 20;
+            Expression<Func<Student, bool>> isTeenAgerExpr = s => isTeenAger(s);
+
+            Func<Student, bool> isTeenAgerComplied = isTeenAgerExpr.Compile();
+
+            //Invoke
+            bool result = isTeenAgerComplied(new Student() { StudentID = 1, StudentName = "Steve", Age = 20 });
+
+            Expression<Action<Student>> printStudentName = s => Console.WriteLine(s.StudentName);
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
