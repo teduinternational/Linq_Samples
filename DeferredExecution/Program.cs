102e06e6283d5025b19d5ecaafaa8756dd90c28d
Bach Ngoc Toan <ngoctoan@live.com>
2019-08-25 22:27:54 +0700

    Deferred execution

---
+++ DeferredExecution/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Models;
+
+namespace DeferredExecution
+{
+    public static class EnumerableExtensionMethods
+    {
+        public static IEnumerable<Student> GetTeenAgerStudents(this IEnumerable<Student> source)
+        {
+
+            foreach (Student std in source)
+            {
+                Console.WriteLine("Accessing student {0}", std.StudentName);
+
+                if (std.Age > 12 && std.Age < 20)
+                    yield return std;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<Student> studentList = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John", Age = 13 } ,
+                new Student() { StudentID = 2, StudentName = "Steve",  Age = 15 } ,
+                new Student() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 12 } ,
+                new Student() { StudentID = 5, StudentName = "Ron" , Age = 21 }
+            };
+            var teenAgerStudents = from s in studentList.GetTeenAgerStudents()
+                select s;
+
+            foreach (Student teenStudent in teenAgerStudents)
+                Console.WriteLine("Student Name: {0}", teenStudent.StudentName);
+
+            Console.Read();
+        }
+    }
+}
