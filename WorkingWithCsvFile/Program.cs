2cbf26468322c61d15003b6e6f8a35b2ae5ed652
Bach Ngoc Toan <ngoctoan@live.com>
2019-08-26 22:34:39 +0700

    Linq to csv

---
+++ WorkingWithCsvFile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WorkingWithCsvFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> strCsv = File.ReadAllLines("Sample.csv");
+
+            var result = from str in strCsv
+                let tmp = str.Split(",").Skip(1).Select(s => Convert.ToInt32(s))
+                select new
+                {
+                    Max = tmp.Max(),
+                    Min = tmp.Min(),
+                    Total = tmp.Sum(),
+                    Avg = tmp.Average()
+                };
+            var query = result.ToList();
+            foreach (var x in query)
+            {
+                Console.WriteLine(
+                    $"Maximum: {x.Max}, " + $"Minimum: {x.Min}, " + $"Total: {x.Total}, " + $"Average: {x.Avg}");
+            }
+
+            Console.Read();
+        }
+    }
+}
