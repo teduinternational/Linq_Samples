c34fd291384bf2979109ad622905d98775fe01cd
Bach Ngoc Toan <ngoctoan@live.com>
2019-08-26 23:04:52 +0700

    Working with XML

---
+++ WorkingWithXml/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace WorkingWithXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XElement xElement = XElement.Load("Employees.xml");
+            var employees = from x in xElement.Elements("Employee")
+                            let city = x.Element("Address")?.Element("City")
+                            where (string)city == "Alta"
+                            orderby city descending
+                            select x;
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee.Element("Name")?.Value);
+                Console.WriteLine(employee.Element("EmpId")?.Value);
+            }
+            Console.Read();
+        }
+    }
+}
