2ebbbf1d88ffef8723de9cc0c0ebcfee1640df6d
Bach Ngoc Toan <ngoctoan@live.com>
2019-08-26 22:51:02 +0700

    Working with JSON

---
+++ WorkingWithJson/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+
+namespace WorkingWithJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //{
+            //    "id": "110355660738", 
+            //    "picture": {
+            //        "data": {
+            //            "url": "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn2/1027085_12033235063_5234302342947_n.jpg", 
+            //            "is_silhouette": false
+            //        }
+            //    }
+            //}
+            string json =
+                "{  \"id\": \"110355660738\",   \"picture\": {    \"data\": {      \"url\": \"https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn2/1027085_12033235063_5234302342947_n.jpg\",       \"is_silhouette\": false    }  }}";
+
+            JObject obj = JObject.Parse(json);
+            //Console.WriteLine((string)obj.picture.data.url);
+
+            var url = obj.Descendants().OfType<JProperty>()
+                .First(p => p.Name == "url").Value.ToString();
+
+            Console.WriteLine(url);
+            Console.Read();
+        }
+    }
+}
